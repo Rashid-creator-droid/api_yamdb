71cac18e5a0bb7351a7735df9dbddcc12325e1b0
Рашид Аюпов <valentaine1234@gmail.com>
2023-01-23 19:43:03 +0300

    Админка джанго.

--- api_yamdb/api/admin.py
+++ api_yamdb/api/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 
-from reviews.models import Category, Genre, Title
+from reviews.models import Category, Genre, Title, TitleGenre
 
 
+class TitleGenreAdmin(admin.TabularInline):
+    model = TitleGenre
+    extra = 1
 
 
-admin.site.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    inlines = (TitleGenreAdmin,)
+
+
+admin.site.register(Title, TitleAdmin)
 admin.site.register(Category)
 admin.site.register(Genre)

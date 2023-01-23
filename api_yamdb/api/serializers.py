036dab15ba73855d55ad9497f6e104ce18d83422
Rickardo-flick <d2soulmate@gmail.com>
2023-01-23 10:55:39 +0300

    fix serial

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -3,11 +3,26 @@ from reviews.models import Comment, Review
 
 
 class ReviewSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+    score = serializers.IntegerField(min_value=1, max_value=10)
 
     class Meta:
         fields = '__all__'
         model = Review
         read_field_only = ('title', )
+
+    def validate(self, data):
+        if self.context["request"].method == "POST":
+            if Review.objects.filter(
+                author=self.context["request"].user,
+                title=self.context["view"].kwargs.get("title_id"),
+            ).exists():
+                raise serializers.ValidationError(
+                    "Нельзя оставить повторный отзыв на одно произведение"
+                )
+        return data
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -19,9 +34,8 @@ class CommentSerializer(serializers.ModelSerializer):
         fields = (
             'id',
             'author',
-            'review',
             'text',
-            'created',
+            'pub_date'
         )
         model = Comment
         read_only_fields = ('review', )
